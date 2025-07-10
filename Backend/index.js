73e822930535db8458e2a97e4507e85e5540399a
Sajida Unnisa <sajidaunnisa24@gmail.com>
2025-07-10 13:20:37 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -6,21 +6,31 @@ const router = require("./routes/index");
 const cookiesParser = require("cookie-parser");
 const { app, server } = require("./socket/index");
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://convosync-chatapp.netlify.app"
+];
+
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
+
 app.use(express.json());
 app.use(cookiesParser());
 
 const PORT = process.env.PORT || 8080;
 
-app.get("/", (request, response) => {
-  response.json({
-    message: "Server running at " + PORT,
-  });
+app.get("/", (req, res) => {
+  res.json({ message: "Server running at " + PORT });
 });
 
 app.use("/api", router);
@@ -30,3 +40,4 @@ connectDB().then(() => {
     console.log("server running at " + PORT);
   });
 });
+
